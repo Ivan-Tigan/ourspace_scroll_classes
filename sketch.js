110a16610fa45c5e79b38d0e98210dc48e3c8a4e
vankata <ivantsoninski@gmail.com>
2023-05-30 19:13:16 +0300

    image scroll

--- sketch.js
+++ sketch.js
@@ -1,45 +1,36 @@
+let images;
+let imageX;
+let image_width = 300
+let scrollSpeed = 1;
+
+function preload() {
+  images = [
+    loadImage('images/1.png'),
+    loadImage('images/2.png'),
+    loadImage('images/3.png'),
+    loadImage('images/1.png'),
+    loadImage('images/2.png'),
+    loadImage('images/3.png'),
+    loadImage('images/1.png'),
+    loadImage('images/2.png'),
+    loadImage('images/3.png'),
+  ];
+  imageX = Array(images.length).fill(0).map((x,i) => i * image_width * 1.1)
+}
 
 
-let colors = ["blue","red","green","yellow","orange","purple","pink","black","white","brown","grey","cyan","magenta"];
-let color = colors[Math.floor((rand()*colors.length))];
-let radius = 30 + Math.floor(rand()*70);
 
 function setup() {
-  const canvas = createCanvas(windowWidth, windowHeight);
-  window.metadata = () => ({
-    name:`Circle #${token_id}`,
-    description: `This is a ${color} circle with a radius of ${radius}.`,
-
-    image: canvas.elt.toDataURL("image/png"),
-    properties: {
-      "Color": color,
-      "Radius": radius
-    },
-    attributes: [
-      {
-        trait_type: "Color",
-        value: color,
-      },
-      {
-        display_type: "number",
-        trait_type: "Radius",
-        value: radius,
-      },
-    ]
-  })
-  console.log(metadata());
+  createCanvas(windowWidth, windowHeight);
 }
-
 
 function draw() {
-  background("ivory");
-  stroke(0);
-  fill(color);
-  circle(width/2,height/2,radius/100 * Math.min(width,height))
+  background(255);
+  for (let i = 0; i < images.length; i++) {
+    images[i].width = image_width
+    images[i].height = 600
+    image(images[i], imageX[i], 0);
+    imageX[i] = (imageX[i] < -image_width * 1.1 ? (images.length - 1) * image_width * 1.1 : imageX[i]) - scrollSpeed
+  }
+  loop();
 }
-
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
